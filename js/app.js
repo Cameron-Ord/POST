7730f81187aa93ef588e263b81e1f01b01a49852
NuckenMcFuggets <cameron.ian.ronald.ord@outlook.com>
2023-04-07 17:15:27 -0700

    first push

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+let section = document.querySelector(`#section_main`);
+
+
+section.insertAdjacentHTML(`beforeend`
+
+
+`
+<article>
+<span class="main_span">
+
+<button class="click">API</button>
+
+
+</span>
+</article>
+`);
+
+function API(){
+
+}
+
+let clicker = document.querySelectorAll(`click`);
+
+
+
+for(let counter = 0; counter < clicker.length; counter + counter +1 ){
+
+
+
+}
+
+
